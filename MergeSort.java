4dfb8d27cf8dff146131f5d0003258612d972c83
AKHIL ADUVALA <aduvalaakhil@gmail.com>
2025-04-17 10:46:32 +0530

    merge sort

--- MergeSort.java
+++ MergeSort.java
@@ -1,35 +1,54 @@
 public class MergeSort {
-    public static void sort(int[] arr, int left, int right){
-        while(left < right){
-            int middle = left + (right - left)/2;
 
-            sort(arr, left, middle);
-            sort(arr, middle + 1, right);
-            merge(arr, left, middle, right);
+    //repeatedly split the array into two halves until there is only one element in each array
+    public static void sort(int[] a, int left, int right){
+        //check if the array has single element or start to call recursively
+        if(left < right){
+            int mid = left + (right - left)/2;
+
+            sort(a, left, mid); //move with left side of the array
+            sort(a, mid + 1, right); //move with right side of the array
+            merge(a, left, mid, right);
         }
     }
-    public static void merge(int[] arr, int left, int middle, int right){
-        int n1 = middle - left + 1;
-        int n2 = right - middle;
 
-        int[] leftArr = new int[n1];
-        int[] rightArr = new int[n2];
+    //take two arrays, merge them as per the crct order(asc/desc)
+    public static void merge(int[] a, int left, int mid, int right){
+        int l = mid - left + 1;
+        int r = right - mid;
 
-        System.arraycopy(arr, left, leftArr, 0, n1);
-        System.arraycopy(arr, middle + 1, rightArr, 0, n2);
+        int[] leftArray = new int[l];
+        int[] rightArray = new int[r];
+
+        System.arraycopy(a, left, leftArray, 0, l); //taking a copy of the left side of array
+        System.arraycopy(a, mid+1, rightArray, 0, r); //taking a copy of the left side of array
 
         int i = 0, j = 0, k = left;
-
-        while(i < k && j < k){
-            arr[k++] = (leftArr[i++] < rightArr[j++] ? leftArr[i++] : rightArr[j++]);
+        //compare both left and right arrays to place elements in crct order
+        while(i < l && j < r){
+            a[k++] = (leftArray[i] < rightArray[j]) ? leftArray[i++] : rightArray[j++];
         }
 
-        while(i < n1){
-            arr[k++] = leftArr[i++];
+        //add elements from left array if any are left
+        while(i < l){
+            a[k++] = leftArray[i++];
         }
 
-        while(j < n2){
-            arr[k++] = rightArr[j++];
+        //add elements from right array if any are left
+        while(j < r){
+            a[k++] = rightArray[j++];
         }
+    }
+
+    public static  void main(String[] args){
+        int[] a = {1,7,6,8,2,3,4};
+        long start = System.nanoTime(); //using nanoTime() instead of currentTimeMillis() as its small dataset
+        sort(a, 0, a.length - 1);
+        long stop = System.nanoTime(); //using nanoTime() instead of currentTimeMillis() as its small dataset
+        System.out.println("Here is array sorted using merge sort");
+        for(int j : a){
+            System.out.println(j);
+        }
+        System.out.println("Time taken for merge sort : " + (stop - start));
     }
 }
